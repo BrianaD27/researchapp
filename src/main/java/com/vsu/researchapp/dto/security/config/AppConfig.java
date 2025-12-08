2baabafdb02b9e7cb4e14d642ae9341b36e1edf5
Quntion <quintonwade25@yahoo.com>
2025-12-08 18:07:13 -0500

    Added Spring Security config and login features

--- src/main/java/com/vsu/researchapp/dto/security/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.vsu.researchapp.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // Add shared @Bean definitions here later if needed
-}
