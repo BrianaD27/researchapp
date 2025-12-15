dc43e300d45917554bc1a1e43b2cf752c650054d
Briana Davis <brianad570@gmail.com>
2025-12-15 18:12:30 -0500

    Added two custom queries to ResearchEventRepository

--- src/main/java/com/vsu/researchapp/domain/repository/ResearchEventRepository.java
+++ src/main/java/com/vsu/researchapp/domain/repository/ResearchEventRepository.java
@@ -1,11 +1,21 @@
 package com.vsu.researchapp.domain.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vsu.researchapp.domain.model.ResearchEvent;
 
 @Repository
 public interface ResearchEventRepository extends JpaRepository<ResearchEvent, Long> {
-    // Add Custom Queries Here
+
+    @Query("SELECT event FROM ResearchEvent event WHERE event.beginDate >= CURRENT_DATE ORDER BY event.beginDate")
+    List<ResearchEvent> getResearchEventByUpcoming();
+
+    @Query("SELECT event FROM ResearchEvent event WHERE event.beginDate >= :earliestDate AND event.endDate <= :latestDate")
+    List<ResearchEvent> getResearchEventsByDateRange(@Param("earliestDate") LocalDate earliestDate, @Param("latestDate") LocalDate latestDate);
 }
