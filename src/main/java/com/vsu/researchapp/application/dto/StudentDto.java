3ccf02c53907ad8dbf41956c2a60c651951bc065
Briana Davis <brianad570@gmail.com>
2026-01-05 17:35:00 -0500

    Updated Dtos

--- src/main/java/com/vsu/researchapp/application/dto/StudentDto.java
+++ src/main/java/com/vsu/researchapp/application/dto/StudentDto.java
@@ -1,22 +1,8 @@
 package com.vsu.researchapp.application.dto;
 
-public class StudentDto {
-    private Long id;
-    private String name;
-    private String email;
-    private String major;
-
-    public StudentDto() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getMajor() { return major; }
-    public void setMajor(String major) { this.major = major; }
-}
+public record StudentDto(
+    Long id,
+    String name,
+    String email,
+    String major
+) {}
