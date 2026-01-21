5d83dfedb2063ed98bdeef6030b8a9968f614607
Briana Davis <brianad570@gmail.com>
2026-01-21 14:19:42 -0500

    Upgraded Java Dtos

--- src/main/java/com/vsu/researchapp/application/dto/CreateResearchEventDto.java
+++ src/main/java/com/vsu/researchapp/application/dto/CreateResearchEventDto.java
@@ -4,21 +4,27 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 public record CreateResearchEventDto(
     String title,
     String description,
     String address,
     String registrationLink,
+
+    @NotBlank(message = "A begin date is required")
     @Schema(example = "2025-12-25", type = "string", format = "date")
     LocalDate beginDate,
     
+    @NotBlank(message = "An end date is required")
     @Schema(example = "2025-12-26", type = "string", format = "date")
     LocalDate endDate,
     
+    @NotBlank(message = "A start time is required")
     @Schema(example = "09:00:00", type = "string", format = "time")
     LocalTime startTime,
     
+    @NotBlank(message = "An end time is required")
     @Schema(example = "17:00:00", type = "string", format = "time")
     LocalTime endTime
 ) {}
--- src/main/java/com/vsu/researchapp/application/dto/CreateStudentDto.java
+++ src/main/java/com/vsu/researchapp/application/dto/CreateStudentDto.java
@@ -1,10 +1,23 @@
 package com.vsu.researchapp.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateStudentDto(
+    @NotBlank(message = "A name is required")
     String name,
+
+    @NotBlank(message = "An email is required")
     String email,
+
+    @NotBlank(message = "A graduate year is required")
     Integer graduateYear,
+
+    @NotBlank(message = "A major is required")
     String major,
+
+    @NotBlank(message = "A description is required")
     String description,
+    
+    @NotBlank(message = "Skills are required")
     String skills
 ) {}
